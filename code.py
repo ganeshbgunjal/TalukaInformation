636dc6424dfb0ee93c098c6c19e28c0c8ffe159f
Ganesh <ganeshgunjal118@gmail.com>
2024-01-05 17:47:02 +0530

    python and sql file add

---
+++ code.py
@@ -0,0 +1,4 @@
+first_name = input("Enter your first name: ")
+middle_name = input("Enter your middle name: ")
+last_name = input("Enter your last name: ")
+population = int(input("Enter population of your village: "))
---
+++ databse.sql
@@ -0,0 +1,5 @@
+show databases
+use db1
+show tables
+
+select * from table_name; 
